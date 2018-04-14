03e8922c13bf7b0fb48afc18ff663efde8377031
Gabriel Joshua Paet <gjvpaet@gmail.com>
2018-04-14 16:20:27 +0800

    Added api

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,8 +1,31 @@
 import * as React from 'react';
+import axios from 'axios';
 
 export interface HelloProps {
     compiler: string;
     framework: string;
 }
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}</h1>;
+// export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}</h1>;
+
+export class Question extends React.Component<any> {
+    constructor(props: any) {
+        super(props);
+
+        this.state ={
+            questions: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('https://opentdb.com/api.php?amount=1&category=10&difficulty=easy&type=multiple').then(response => {
+            // console.log(response);
+            this.setState({questions: response.data});
+        });
+    }
+
+    render() {
+        console.log(this.state);
+        return <h1>sadasd</h1>;
+    }
+}
